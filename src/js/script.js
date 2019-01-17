49c7fffc7dc7ad6f7b2e61b702c48c9d05a67806
Brianeto <bdmb45@gmail.com>
2019-01-17 18:58:51 -0500

    in process

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,3 @@
-// COUNTER
-$(window).scroll(startCounter);
-    function startCounter() {
-    var hT = $('.js-numero-contadores').offset().top,
-        hH = $('.js-numero-contadores').outerHeight(),
-        wH = $(window).height();
-        if ($(window).scrollTop() > hT+hH-wH) {
-            $(window).off("scroll", startCounter);
-            $('.js-numero-contadores').each(function () {
-                var $this = $(this);
-                jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-                    duration: 2000,
-                    easing: 'swing',
-                    step: function () {
-                        $this.text(Math.ceil(this.Counter));
-                    }
-                });
-            });
-        }
-    }
-
 // CAROUSEL
 $('.owl-carousel').owlCarousel({
     nav:true,
@@ -31,8 +10,9 @@ $('.owl-carousel').owlCarousel({
         600:{
             items:3
         },
-        1000:{
+       1000:{
             items:5
         }
     }
 })
+$("#carouselExampleSlidesOnly").function();
